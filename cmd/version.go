a3ff04fa97b7ee2edcd1ca5f641ad42ecdc62710
Nao1215 <n.chika156@gmail.com>
2022-02-05 15:40:38 +0900

    Update version

--- cmd/version.go
+++ cmd/version.go
@@ -2,19 +2,17 @@ package cmd
 
 import (
 	"fmt"
-	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	// Version is subaru command version.
-	Version = "0.0.1"
+	Version = "0.1.0"
 )
 
 func getVersion() string {
-	return fmt.Sprintf("Version: %s\nOS     : %s\nArch   : %s",
-		Version, runtime.GOOS, runtime.GOARCH)
+	return fmt.Sprintf("subaru version " + Version + " (under Apache License verison 2.0)")
 }
 
 var versionCmd = &cobra.Command{
